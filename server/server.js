4043805ccf9ead400a76d68c56cd6ae8271a090d
Mason Howe <mason.a.howe@gmail.com>
2021-04-20 20:03:32 -0600

    add client server folder

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
 const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3030;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//static asset here
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
