eddf17187b35d5a81e60bda7dbec625a7d851f38
Mason Howe <mason.a.howe@gmail.com>
2021-04-22 19:56:54 -0600

    add socket io

--- server/server.js
+++ server/server.js
@@ -1,23 +1,31 @@
 const express = require("express");
-const db = require('./config/connection');
-const { authMiddleware } = require('./utils/auth');
-const path = require('path');
+const db = require("./config/connection");
+const { authMiddleware } = require("./utils/auth");
+const path = require("path");
 
 // import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require("apollo-server-express");
 
 // import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require("./schemas");
 
 const PORT = process.env.PORT || 3030;
 const app = express();
+
+// create socket.io server
+const socketServer = require("http").createServer(app);
+const io = require("socket.io")(socketServer);
+io.on("connection", () => {});
+socketServer.listen(3006);
 
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
+  context: authMiddleware,
 });
+
+//create socket.io
 
 // integrate Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
@@ -27,7 +35,7 @@ app.use(express.json());
 
 //static asset here
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
