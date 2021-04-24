77c668e08e0c11871b8f1ce17d21ee39fdd40985
Mason Howe <mason.a.howe@gmail.com>
2021-04-24 15:42:00 -0600

    add socket.io and client

--- server/server.js
+++ server/server.js
@@ -8,15 +8,12 @@ const { ApolloServer } = require("apollo-server-express");
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
+const { isObjectType } = require("graphql");
 
 const PORT = process.env.PORT || 3030;
 const app = express();
-
-// create socket.io server
-const socketServer = require("http").createServer(app);
-const io = require("socket.io")(socketServer);
-io.on("connection", () => {});
-socketServer.listen(3006);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
@@ -34,9 +31,14 @@ app.use(express.json());
 //static asset here
 
 db.once("open", () => {
-  app.listen(PORT, () => {
+  http.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
+});
+
+//socket io
+io.on("connection", (socket) => {
+  console.log("new client connected");
 });
