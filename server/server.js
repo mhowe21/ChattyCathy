c23c650d4cc3541d4a882b5c7f615146d4c492c7
Mason Howe <mason.a.howe@gmail.com>
2021-04-28 22:04:01 -0600

    add static assests I

--- server/server.js
+++ server/server.js
@@ -35,6 +35,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //static asset here
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 //db
 // db.once("open", () => {
