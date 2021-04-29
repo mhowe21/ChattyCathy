b200bb33ff57dc0138176db6491dc6f578eeff15
Mason Howe <mason.a.howe@gmail.com>
2021-04-28 21:23:50 -0600

    comment db out

--- server/server.js
+++ server/server.js
@@ -36,12 +36,15 @@ app.use(express.json());
 
 //static asset here
 
-db.once("open", () => {
-  http.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
+//db
+// db.once("open", () => {
+
+// });
+
+http.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}!`);
+  // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
 
 //socket io
