05c945ce65b3e191bfdc38828aae855a212afc7f
Mason Howe <mason.a.howe@gmail.com>
2021-05-02 13:55:09 -0600

    remove host

--- client/src/contexts/SocketProvider.js
+++ client/src/contexts/SocketProvider.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import io from "socket.io-client";
-const HOST = `${window.location.hostname}:9352/socket.io/?EIO=4&transport=websocket`;
+//const HOST = `${window.location.hostname}:9352/socket.io/?EIO=4&transport=websocket`;
 
 const SocketContext = React.createContext();
 
@@ -12,7 +12,7 @@ export function SocketProvider({ id, children }) {
   const [socket, setSocket] = useState();
 
   useEffect(() => {
-    const newSocket = io(HOST, {
+    const newSocket = io({
       query: { id },
       extraHeaders: {
         "my-custom-header": "abcd",
