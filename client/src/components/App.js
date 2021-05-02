6438543bb0827519d07008ab2d38e18b7cfd413c
Russ <ocjade@comcast.net>
2021-05-02 13:17:29 -0600

    fixed apollo error for login

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,10 +8,40 @@ import { SocketProvider } from "../contexts/SocketProvider";
 import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
 
-const client = new ApolloClient();
+
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Berrer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
+
+
+// function App() {
+//   const [id, setId] = localStorage.getItem('id_token');
+//   return (
+//     <ApolloProvider client={client}>
+//       <SocketProvider id={id}>
+//         <ContactsProvider>
+//           <ConversationsProvider id={id}>
+//             <Dashboard id={id} />
+//           </ConversationsProvider>
+//         </ContactsProvider>
+//       </SocketProvider>
+//     </ApolloProvider >
+//   );
+// }
+
 
 function App() {
-  const [id, setId] = useLocalStorage("id");
+  const [id, setId] = useLocalStorage("id_token");
 
   const dashboard = (
     <ApolloProvider client={client}>
@@ -25,7 +55,19 @@ function App() {
     </ApolloProvider>
   );
 
-  return id ? dashboard : <Login onIdSubmit={setId} />;
+  const login = (
+    <ApolloProvider client={client}>
+      <SocketProvider id={id}>
+        <ContactsProvider>
+          <ConversationsProvider id={id}>
+            <Login onIdSubmit={setId} />
+          </ConversationsProvider>
+        </ContactsProvider>
+      </SocketProvider>
+    </ApolloProvider>
+  );
+
+  return id ? dashboard : login;
 }
 
 export default App;
